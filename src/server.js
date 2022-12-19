46c11c0e02ce6296a71d7dd5d7b35967081e9612
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-12-19 14:28:07 -0300

    feat: add connection verification

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 const { app } = require("./app");
+const connection = require("./database/connection");
 
 const PORT = 3000;
 
-app.listen(PORT, console.log(`Server up and running on port ${PORT}`));
+app.listen(PORT, async () => {
+  try {
+    await connection.execute("SELECT 1");
+    console.log(`Server up and running on port ${PORT}`);
+  } catch (err) {
+    console.error("Database not connected!");
+    process.exit(0);
+  }
+});
